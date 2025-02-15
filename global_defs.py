8ce9221640ee675647e913248fadfd15695e39c1
xcheng-tsinghua <786682191@qq.com>
2025-02-15 20:16:41 +0800

    change

--- global_defs.py
+++ global_defs.py
@@ -15,22 +15,3 @@ pen_up = 16  # 自建机械草图
 pen_down = 17  # 自建机械草图
 # pen_down = 1  # quickdraw
 
-
-# 版本: 1.96.2 (user setup)
-# 提交: fabdb6a30b49f79a7aba0f2ad9df9b399473380f
-# 日期: 2024-12-19T10:22:47.216Z
-# Electron: 32.2.6
-# ElectronBuildId: 10629634
-# Chromium: 128.0.6613.186
-# Node.js: 20.18.1
-# V8: 12.8.374.38-electron.0
-# OS: Windows_NT x64 10.0.22631
-#
-# https://vscode.download.prss.microsoft.com/dbazure/download/stable/fabdb6a30b49f79a7aba0f2ad9df9b399473380f/vscode-server-linux-x64.tar.gz
-# https://vscode.download.prss.microsoft.com/dbazure/download/stable/fabdb6a30b49f79a7aba0f2ad9df9b399473380f/vscode_cli_alpine_x64_cli.tar.gz
-#
-# mv server ~/.vscode-server/cli/servers/Stable-fabdb6a30b49f79a7aba0f2ad9df9b399473380f/
-# mv code ~/.vscode-server/code-fabdb6a30b49f79a7aba0f2ad9df9b399473380f
-#
-# ssh -p 25773 root@219.223.181.7
-# ssh -p 25773 root@219.223.181.7
