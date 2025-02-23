e05dd0b4ad51b66abfa484eec861ca03e14a7db3
xcheng-tsinghua <786682191@qq.com>
2025-02-23 21:59:10 +0800

    change

--- data_utils/sketch_vis.py
+++ data_utils/sketch_vis.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
 
     # ahead, ext = os.path.splitext(r'D:\document\DeepLearning\DataSet\unified_sketch_from_quickdraw\train\apple\177.txt')
 
-    vis_sketch_unified(r'D:\document\DeepLearning\DataSet\unified_sketch_cad_stk32_stkpnt32\train\Bearing\00f45ba0a8f47a198aaa6ef095cb71a4_5.txt')
+    vis_sketch_unified(r'D:\document\DeepLearning\DataSet\unified_sketch_cad_stk32_stkpnt32\train\Gear\0dd1520e215d8d4c3cdbfe889316ba33_2.txt')
 
     pass
 
