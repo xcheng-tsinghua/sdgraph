f943e33bc89b04b95da36c46834aa9b85052ae67
xcheng-tsinghua <786682191@qq.com>
2025-03-13 08:51:43 +0800

    change

--- encoders/sdgraph.py
+++ encoders/sdgraph.py
@@ -34,12 +34,12 @@ class SDGraphCls(nn.Module):
         # 利用 sdgraph 更新特征
         self.sd1 = SDGraphEncoder(sparse_l0, sparse_l1, dense_l0, dense_l1,
                                   self.n_stk, self.n_stk_pnt,
-                                  dropout=dropout
+                                  dropout=0.0
                                   )
 
         self.sd2 = SDGraphEncoder(sparse_l1, sparse_l2, dense_l1, dense_l2,
                                   self.n_stk, self.n_stk_pnt // 2,
-                                  dropout=dropout
+                                  dropout=0.0
                                   )
 
         # 利用输出特征进行分类
