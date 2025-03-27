bda8d98d70395bc6e2e338ec2ce03b36d8854d73
xcheng-tsinghua <786682191@qq.com>
2025-03-27 11:15:15 +0800

    change

--- train_cls.py
+++ train_cls.py
@@ -11,7 +11,7 @@ import os
 import global_defs
 # 自建模块
 from data_utils.SketchDataset import SketchDataset
-from encoders.sdgraph2 import SDGraphCls as SDGraphCls
+from encoders.sdgraph2 import SDGraphCls2 as SDGraphCls
 # from encoders.sdgraph_valid_bk import SDGraph as SDGraphCls
 from encoders.utils import inplace_relu, clear_log, clear_confusion, all_metric_cls
 
