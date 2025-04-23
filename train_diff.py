4b57844e19ca0c0e752ba4654879a84451095251
xcheng-tsinghua <786682191@qq.com>
2025-04-23 21:44:48 +0800

    change

--- train_diff.py
+++ train_diff.py
@@ -81,8 +81,8 @@ def main(args):
             data_root = args.root_local
         else:
             data_root = args.root_sever
-        # train_dataset = QuickDrawDiff(root=data_root)
-        train_dataset = DiffDataset(root=data_root)
+        train_dataset = QuickDrawDiff(root=data_root)
+        # train_dataset = DiffDataset(root=data_root)
         train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.bs, shuffle=True, num_workers=4)
 
         '''优化器'''
