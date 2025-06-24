eae8cb0c29ac0f2fcb6324a22b2285f846c13a11
xcheng-tsinghua <786682191@qq.com>
2025-06-24 14:50:33 +0800

    change

--- data_utils/sketch_dataset.py
+++ data_utils/sketch_dataset.py
@@ -71,7 +71,7 @@ class SketchDatasetCls(Dataset):
                  coor_mode='ABS',
                  max_len=200,
                  img_size=(224, 224),
-                 workers=4,
+                 workers=8,
                  is_retrieval=False,
                  is_already_divided=False,
                  is_preprocess=True,
