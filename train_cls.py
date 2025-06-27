3a82f16948af1cbb33c73a9bce1f9944efb7f48e
xcheng-tsinghua <786682191@qq.com>
2025-06-27 15:59:14 +0800

    change

--- train_cls.py
+++ train_cls.py
@@ -21,7 +21,7 @@ import global_defs
 def parse_args():
     parser = argparse.ArgumentParser('training')
 
-    parser.add_argument('--bs', type=int, default=15000, help='batch size in training')
+    parser.add_argument('--bs', type=int, default=1000, help='batch size in training')
     parser.add_argument('--epoch', default=1000, type=int, help='number of epoch in training')
     parser.add_argument('--lr', default=1e-4, type=float, help='learning rate in training')
     parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
@@ -30,7 +30,7 @@ def parse_args():
     parser.add_argument('--is_preprocess', type=str, default='True', choices=['True', 'False'])
     parser.add_argument('--local', default='False', choices=['True', 'False'], type=str)
     parser.add_argument('--coor_mode', type=str, default='REL', choices=['ABS', 'REL'], help='absolute coordinate or relative coordinate')
-    parser.add_argument('--model', type=str, default='SketchRNN', choices=['SketchRNN', 'SketchTransformer', 'SDGraph'])
+    parser.add_argument('--model', type=str, default='SketchTransformer', choices=['SketchRNN', 'SketchTransformer', 'SDGraph'])
 
     parser.add_argument('--root_sever', type=str, default=rf'/opt/data/private/data_set/quickdraw/MGT/log_normal_mean')
     parser.add_argument('--root_local', type=str, default=rf'D:\document\DeepLearning\DataSet\quickdraw\MGT\log_normal_mean')
