9bb50e69334827c88284f909f0c41c8aff8a8184
xcheng-tsinghua <786682191@qq.com>
2025-07-04 11:55:28 +0800

    change

--- train_cls.py
+++ train_cls.py
@@ -28,7 +28,7 @@ def parse_args():
     parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
     parser.add_argument('--is_load_weight', type=str, default='False', choices=['True', 'False'])
     parser.add_argument('--is_shuffle_stroke', type=str, default='True', choices=['True', 'False'])
-    parser.add_argument('--is_preprocess', type=str, default='True', choices=['True', 'False'])
+    parser.add_argument('--is_preprocess', type=str, default='False', choices=['True', 'False'])
     parser.add_argument('--local', default='False', choices=['True', 'False'], type=str)
     parser.add_argument('--coor_mode', type=str, default='REL', choices=['ABS', 'REL'], help='absolute coordinate or relative coordinate')
     parser.add_argument('--model', type=str, default='MGT', choices=['SketchRNN', 'SketchTransformer', 'SDGraph', 'MGT'])
